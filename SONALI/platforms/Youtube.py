c640115e3d1e15d9733ee83a0cc4ac329fb3baec
Gyanesh Patel <gyaneshpatel605@gmail.com>
2025-12-09 02:39:43 +0530

    Update Youtube.py

--- SONALI/platforms/Youtube.py
+++ SONALI/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from py_yt import VideosSearch
-from Sonali.utils.database import is_on_off
-from Sonali.utils.formatters import time_to_seconds
+from SONALI.utils.database import is_on_off
+from SONALI.utils.formatters import time_to_seconds
 import os
 import glob
 import random
@@ -596,4 +596,5 @@ class YouTubeAPI:
             direct = True
             downloaded_file = await download_song(link)
         return downloaded_file, direct
+
 
